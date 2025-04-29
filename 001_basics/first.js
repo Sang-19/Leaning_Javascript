b7effb96fb135c8d635c0a4c8eeaacb5d5726e9b
Sangbed <83353411+Sang-19@users.noreply.github.com>
2025-04-29 13:46:47 +0000

    hello

--- 001_basics/first.js
+++ 001_basics/first.js
@@ -1,1 +1,2 @@
-console.log("Hello");
+console.log("Why do u want to learn javascript?");
+console.log("If u want learn it for building something then u can learn otherwise if u want for jobb then don't learn.");
