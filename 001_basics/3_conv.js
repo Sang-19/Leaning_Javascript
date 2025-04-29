ae91da031538b23639b7895971292ce10999eef5
Sangbed <83353411+Sang-19@users.noreply.github.com>
2025-04-29 14:27:11 +0000

    7%

---
+++ 001_basics/3_conv.js
@@ -0,0 +1,46 @@
+/*let score = 33
+console.log(typeof score);
+console.log(typeof(score));
+let va = Number(score);
+console.log(va);
+*/
+//NaN - never believe in numbber
+//
+//33abc - NaN
+// u can convert number to boolean also like 1 to true and 0 to false
+// empty -- false - boolean
+let va = "";
+//console.log(Boolean(va));
+
+
+
+
+//Get negative value
+
+let s = 32
+let ne = -s
+//console.log(ne)
+
+
+//console.table([2+2, 2*3 , 2/4])
+
+
+//concatanation of string
+
+
+
+//console.log("1" + 2);
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "3");
+//don't depend like this
+
+console.log(+true)  // true + not possible
+//+'' = 1
+
+//focus onnn readibility not on writing in shortcut
+
+
+
+let gamecounter = 100;
+++gamecounter;
+console.log(gamecounter);
