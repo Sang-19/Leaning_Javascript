318e5ef573447a512310d29ccbf28064f4ce8854
Sangbed <83353411+Sang-19@users.noreply.github.com>
2025-04-29 13:55:53 +0000

    4%done

--- 001_basics/first.js
+++ 001_basics/first.js
@@ -1,2 +1,17 @@
 console.log("Why do u want to learn javascript?");
 console.log("If u want learn it for building something then u can learn otherwise if u want for jobb then don't learn.");
+
+
+const accountId = 234 ;
+let acr = "hello";
+var ap = "32324";
+accit = "jaipur";
+
+acr = "ewr";
+ap = "234";
+accit = "iouwhkoi";
+
+
+/* pprefer not to use var because */
+
+console.table([acr, ap, accit]);
