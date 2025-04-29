54ffc09a572a525e7c7a36fb1c1f3b02530410e3
Sangbed <83353411+Sang-19@users.noreply.github.com>
2025-04-29 14:07:57 +0000

    datatypes

--- 001_basics/2_datatypes.js
+++ 001_basics/2_datatypes.js
@@ -2,5 +2,16 @@
 
 
 //alert(3+3) // we are using node js, not browser
+// https://tc39.es/   - main javascript documentation
+
+let name = "ds";
+//number = 2 to power 53
+//bigInt = 
+//sstring
+//boolean = true and false
+//null = stand alone value -- typeOf -- object
+//undefined == not assign --typeOf -- undefined
+//symbol = when using react -- uniqueness ke liye 
+//typeOf  
 
 
