a67a32ba3f9aa0a8c4be26459c354012f28f228c
Sangbed <83353411+Sang-19@users.noreply.github.com>
2025-04-29 14:00:10 +0000

    4% done

---
+++ 001_basics/1_variables.js
@@ -0,0 +1,19 @@
+console.log("Why do u want to learn javascript?");
+console.log("If u want learn it for building something then u can learn otherwise if u want for jobb then don't learn.");
+
+
+const accountId = 234 ;
+let acr = "hello";
+var ap = "32324";
+accit = "jaipur";
+
+acr = "ewr";
+ap = "234";
+accit = "iouwhkoi";
+
+
+/*  prefer not to use var because
+    of issue in block scope and functional scope
+*/
+
+console.table([acr, ap, accit]);
